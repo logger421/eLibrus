7ec5a878615fe157507c654e31b6c7ba6ea9b49c
ffffffffffffffff0 <grtmt@interia.pl>
2023-01-14 19:36:35 +0100

    redirecty w zaleznosci od roli;

--- eLibrus/helpers/redirect_role.js
+++ eLibrus/helpers/redirect_role.js
@@ -1,22 +1,51 @@
 var express = require('express');
 
-const checkRole = (req, res) => {
-    console.log(req.user);
-    if (!req.user) res.redirect('/home');
-    else {
-        if (req.user.dataValues.rola == 1) {
-            res.redirect('/student');
-        }
-        else if (req.user.dataValues.rola == 2) {
-            res.redirect('/parent');
-        }
-        else if (req.user.dataValues.rola == 3) {
-            res.redirect('/teacher');
-        }
-        else if (req.user.dataValues.rola == 4) {
-            res.redirect('/administrator');
-        }
+const isStudent = (req, res, next) => {
+    if (!req.user) res.redirect('/');
+    else if (req.user.dataValues.rola == 1) {
+        next();
     }
+    else {
+        res.redirect('/');
+    }
+};
+
+const isParent = (req, res, next) => {
+    if (!req.user) res.redirect('/');
+    else if (req.user.dataValues.rola == 2) {
+        next();
+    }
+    else {
+        res.redirect('/');
+    }
+};
+
+const isTeacher = (req, res, next) => {
+    if (!req.user) res.redirect('/');
+    else if (req.user.dataValues.rola == 3) {
+        next();
+    }
+    else {
+        res.redirect('/');
+    }
+};
+
+const isAdmin = (req, res, next) => {
+    if (!req.user) res.redirect('/');
+    else if (req.user.dataValues.rola == 4) {
+        next();
+    }
+    else {
+        res.redirect('/');
+    }
+};
+
+const redirectWithRole = (req, res) => {
+    if (!req.user) res.redirect('/home');
+    else if (req.user.dataValues.rola == 1) res.redirect('/student');
+    else if (req.user.dataValues.rola == 2) res.redirect('/parent');
+    else if (req.user.dataValues.rola == 3) res.redirect('/teacher');
+    else if (req.user.dataValues.rola == 4) res.redirect('/admin');
 }
 
-module.exports = { checkRole };
+module.exports = { isStudent, isParent, isTeacher, isAdmin, redirectWithRole };
