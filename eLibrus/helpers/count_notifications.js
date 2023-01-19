654921e0a7c7f731bc2b0b4f2d9e5953037018c3
ffffffffffffffff0 <grtmt@interia.pl>
2023-01-19 20:03:38 +0100

    brak powiadomien w powiadomieniach

--- eLibrus/helpers/count_notifications.js
+++ eLibrus/helpers/count_notifications.js
@@ -3,7 +3,7 @@ const { sequelize } = require('../models');
 const count_notifications = async (user_id) => {
     const [notif] = await sequelize.query(`
         SELECT * FROM wiadomosc
-        WHERE typ = '2' AND odbiorca_id = ?
+        WHERE typ = '2' AND odbiorca_id = ? AND odczytana = 0
     `, {
         replacements: [user_id]
     }); 
