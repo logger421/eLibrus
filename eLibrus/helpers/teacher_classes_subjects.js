ed344b4ceea4dbbaf0b16345945c73b3951bf42e
logger421 <wojteklotko3@gmail.com>
2023-01-19 19:53:58 +0100

    Updated sequelize queries to use prepared statemets

--- eLibrus/helpers/teacher_classes_subjects.js
+++ eLibrus/helpers/teacher_classes_subjects.js
@@ -3,8 +3,10 @@ const sequelize = require('../models').sequelize;
 async function getClass(id) {
     const [result, meta] = await sequelize.query(`
         SELECT distinct klasa_id FROM zajecia 
-        WHERE prowadzacy_id = ${id}
-    `)
+        WHERE prowadzacy_id = ?
+    `,{
+        replacements: [id]
+    })
     return result;
 };
 
@@ -12,8 +14,10 @@ async function getSubject (id, class_id) {
     const [result, meta] = await sequelize.query(`
         SELECT zajecia_id, przedmioty.nazwa FROM zajecia 
         NATURAL JOIN przedmioty
-        WHERE prowadzacy_id = ${id} AND klasa_id = ${class_id}
-    `)
+        WHERE prowadzacy_id = ? AND klasa_id = ?
+    `,{
+        replacements: [id, class_id]
+    })
     return result;
 };
 
